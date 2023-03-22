5251a57b00ae908421152412f16acb7b993f48f9
marianasfc <a22001926@alunos.ulht.pt>
2023-03-22 23:21:07 +0000

    1trabalho

--- core/views.py
+++ core/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import redirect, render
-from .models import Topico, Conteudo, Conversor, Limite
-from .forms import TopicoForm, ConteudoForm, ConversorForm, LimiteForm
-from .funcoes import calculateNyquist, conversor
+
 
 # Create your views here.
 def index_view(request):
